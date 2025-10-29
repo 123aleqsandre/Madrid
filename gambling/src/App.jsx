849435b9e3ce60d7c03c2b60a25a9519874eda4d
123aleqsandre <147233725+123aleqsandre@users.noreply.github.com>
2025-10-29 19:58:26 +0400

    gambling reinforce

--- gambling/src/App.jsx
+++ gambling/src/App.jsx
@@ -1,7 +1,23 @@
-import Player from "./components/Player";
+import { useState } from "react";
+import Player from "./components/Player.jsx";
 
 function App() {
-  const dicetmojis = ["🐺", "🐻", "🦁", "🐢", "🐊", "🐉"]
+  const [player1, setPlayer1] = useState(0);
+  const [player2, setPlayer2] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+
+  const rollDice = () => {
+    const RandomNumber = Math.floor(Math.random() * 6) + 1;
+
+    if (currentPlayer === 1) {
+      setPlayer1(RandomNumber);
+      setCurrentPlayer(2);
+    } else {
+      setPlayer2(RandomNumber);
+      setCurrentPlayer(1);
+    }
+  };
+
   return (
     <div
       style={{
@@ -13,13 +29,22 @@ function App() {
       <h1>Winner takes all</h1>
 
       <div>
-        <Player title={"player 1"}/>
+        <Player
+          title={"player 1"}
+          num={player1}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 2}
+        />
 
         <div>VS</div>
 
-        <Player title={"player 2"}/>
+        <Player
+          title={"player 2"}
+          num={player2}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 1}
+        />
       </div>
-
     </div>
   );
 }
--- gambling/src/components/Player.jsx
+++ gambling/src/components/Player.jsx
@@ -1,9 +1,13 @@
-export default function Player({title}) {
+export default function Player({ title, num, handleClick, isDisabled }) {
+  const dicetmojis = ["🐺", "🐻", "🦁", "🐢", "🐊", "🐉"];
+
   return (
     <div>
       <h3> {title} </h3>
-      <div style={{ fontSize: "60px" }}>🎲</div>
-      <p>1</p>
+      <div style={{ fontSize: "60px" }}>{dicetmojis[num - 1]}</div>
+      <button onClick={handleClick} disabled={isDisabled}>
+        Gambling
+      </button>
     </div>
   );
 }
