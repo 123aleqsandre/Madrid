6b4d411248125d3e54b109775a563e797baba43a
123aleqsandre <147233725+123aleqsandre@users.noreply.github.com>
2025-11-23 12:35:46 +0400

    new code

--- daily-todos/src/App.jsx
+++ daily-todos/src/App.jsx
@@ -1,16 +1,64 @@
-import "./App.css";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
-function TodoApp() {
+import "./App.css";
+
+export default function App() {
   const [todos, setTodos] = useState([]);
-}
-const presetTodos = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Learn React" },
-  { id: 3, text: "Go for a walk" },
-];
 
-function newTodo() {
-  setTodos((prev) => [...prev, newTodo]);
-}
+  const TodosList = [
+    { id: nanoid(), text: "ვუყურებთ როგორ აგებს ბარსა რეალთან" },
+    { id: nanoid(), text: "რეალმა გაანადგურა 11-0(ისტორია გაამეორა)" },
+    { id: nanoid(), text: "მივდივართ მზიურში" },
+    { id: nanoid(), text: "ვესალმებით ჯიგარ მასწავლებალს" },
+    { id: nanoid(), text: "ვახსენებთ ბარსას წაგებებს" },
+    { id: nanoid(), text: "თუ დიალოგში აგვყვა ისტორიაში ბარსას ყველა წაგებას ვახსენებთ" },
+    { id: nanoid(), text: "და ტკბილად ვაგრძელებთ ცხოვრებას(რეალი არ აგებს)(ვერ)" },
+  ];
+  const addTodo = (text) => {
+    const newTodo = { id: nanoid(), text };
+    setTodos([...todos, newTodo]);
+  };
 
-export default App;
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+
+  return (
+    <div className="Start">
+      <h1>Todo List</h1>
+
+      <div className="todo-list">
+        {todos.length === 0 && (
+          <div className="empty">list is empty</div>
+        )}
+
+        {todos.length > 0 && (
+          <>
+            {todos.map((todoAll) => (
+              <div className="item" key={todoAll.id}>
+                <p>{todoAll.text}</p>
+                <button onClick={() => deleteTodo(todoAll.id)}>delete</button>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+
+      <div className="buttons">
+        <h2>I am not evil just love madrid</h2>
+
+        <div className="container">
+          {TodosList.map((press) => (
+            <button
+              key={press.id}
+              className="preset-button"
+              onClick={() => addTodo(press.text)}
+            >
+              {press.text}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- daily-todos/src/main.jsx
+++ daily-todos/src/main.jsx
@@ -2,7 +2,4 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
 );
