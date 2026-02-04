827fdc87094b2f21e18f9a05b3e15875c7abd454
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-02-04 20:06:36 +0400

    .

--- QuizGame/src/App.jsx
+++ QuizGame/src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from "react";
-import QuizQuestions from "./components/QuizQuestions"
-import {QUIZ_DATA} from "./quiz-questions"
+import QuizQuestions from "./components/QuizQuestions";
 
 function App() {
   const [difficulty, setDifficulty] = useState(null);
 
-  if(difficulty !== null){
-    return <QuizQuestions/>
+  if (difficulty !== null) {
+    return <QuizQuestions />;
   }
 
   return (
     <>
-    <h1>Welcome to our game</h1>
-    <h2>choose the difficulty</h2>
-    <button onClick={() => 
-    {setDifficulty("easy") 
-    console.log(difficulty)}}
-    >easy</button>
+      <h1>Welcome to our game</h1>
+      <h2>choose the difficulty</h2>
+      <button
+        onClick={() => {
+          setDifficulty("easy");
+          console.log(difficulty);
+        }}
+      >
+        easy
+      </button>
 
-    <button onClick={() => 
-    {setDifficulty("medium") 
-    console.log(difficulty)}}
-    >medium</button>
+      <button
+        onClick={() => {
+          setDifficulty("medium");
+          console.log(difficulty);
+        }}
+      >
+        medium
+      </button>
 
-    <button onClick={() => 
-    {setDifficulty("hard") 
-    console.log(difficulty)}}
-    >hard</button>
+      <button
+        onClick={() => {
+          setDifficulty("hard");
+          console.log(difficulty);
+        }}
+      >
+        hard
+      </button>
     </>
-  )
+  );
 }
 
 export default App;
--- QuizGame/src/components/QuizQuestions.jsx
+++ QuizGame/src/components/QuizQuestions.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { QUIZ_DATA } from "../quiz-questions.js";
 export default function QuizQuestions() {
-  const CurrentQuestion = QUIZ_DATA[0];
+  const [index, setIndex] = useState(0);
+  const CurrentQuestionData = QUIZ_DATA[index];
+              if (QUIZ_DATA.lenght === index) {
+                return <h1>game over</h1>;
+              }
   return (
     <>
-      <h1>{CurrentQuestion.question}</h1>
-      {
-        (CurrentQuestion.answers.map = (item) => {
-          return <p>{item}</p>;
-        })
-      }
+      <h1>{CurrentQuestionData.question}</h1>
+      {CurrentQuestionData.answers.map((item, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => {
+              if (CurrentQuestionData.correct === index) {
+                console.log(CurrentQuestionData.correct);
+              } else {
+                console.log("bro kill yourself");
+              }
+              setIndex((prev) => prev + 1);
+
+
+            }}
+          >
+            {item}
+          </button>
+        );
+      })}
     </>
   );
 }
