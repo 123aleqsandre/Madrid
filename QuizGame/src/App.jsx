4d6766354b6e1277ad0e6269b2c2a8fa50121d3c
123aleqsandre <147233725+123aleqsandre@users.noreply.github.com>
2026-02-09 19:07:59 +0400

    .

--- QuizGame/src/App.jsx
+++ QuizGame/src/App.jsx
@@ -1,46 +1,36 @@
 import { useState } from "react";
-import QuizQuestions from "./components/QuizQuestions";
-import "./App.css";
+import QuizQuestions from "./components/QuizQuestions"
+import QUIZ_DATA from "./quiz-questions"
+
 
 function App() {
   const [difficulty, setDifficulty] = useState(null);
 
-  if (difficulty !== null) {
-    return <QuizQuestions />;
+  if(difficulty !== null){
+    return <QuizQuestions/>
   }
 
   return (
     <>
-      <h1>Welcome to our game</h1>
-      <h2>choose the difficulty</h2>
-      <button
-        onClick={() => {
-          setDifficulty("easy");
-          console.log(difficulty);
-        }}
-      >
-        easy
-      </button>
+    <h1>Welcome to our game</h1>
+    <h2>choose the difficulty</h2>
+    <button onClick={() => 
+    {if(setDifficulty = "easy"){
+      return QUIZ_DATA.difficulty
+    }}}
+    >easy</button>
 
-      <button
-        onClick={() => {
-          setDifficulty("medium");
-          console.log(difficulty);
-        }}
-      >
-        medium
-      </button>
+    <button onClick={() => 
+    {setDifficulty("medium") 
+    console.log(difficulty)}}
+    >medium</button>
 
-      <button
-        onClick={() => {
-          setDifficulty("hard");
-          console.log(difficulty);
-        }}
-      >
-        hard
-      </button>
+    <button onClick={() => 
+    {setDifficulty("hard") 
+    console.log(difficulty)}}
+    >hard</button>
     </>
-  );
+  )
 }
 
 export default App;
